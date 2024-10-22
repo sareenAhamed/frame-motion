ce7aa2a3faa83b8dcdca250551ccc21d659ecfab
sareenAhamed <sareenahamed40@gmail.com>
2024-10-22 14:08:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+// motion
+import { motion } from 'framer-motion'
+
+// variants
+import { fadeIn } from './variants.js'
+
+function App() {
+ 
+
+  return (
+    <>
+    <motion.div 
+      variants={fadeIn("up", 0.2)} 
+      initial = "hidden" 
+      whileInView={"show"} 
+      viewport={{once: false, amount: 0.7}}>
+
+      <h2>Text 1</h2>
+      <h2>Text 2</h2>
+      <h2>Text 3</h2>
+      <h2>Text 4</h2>
+    </motion.div>
+      
+      
+
+      
+      
+    </>
+  )
+}
+
+export default App
